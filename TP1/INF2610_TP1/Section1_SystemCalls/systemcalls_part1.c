9bf2c10685738a97bdec0689aafec5933b6ae5d0
Andrew <anchaam>
2025-01-20 13:53:46 -0500

    systemcalls_output1 DONE systemcalls_part1 DONE.

--- TP1/INF2610_TP1/Section1_SystemCalls/systemcalls_part1.c
+++ TP1/INF2610_TP1/Section1_SystemCalls/systemcalls_part1.c
@@ -6,8 +6,33 @@
  * Ajoutez vos noms, prénoms et matricules
 */
 
+#include <unistd.h>
+#include <fcntl.h>
+#include <stdio.h>
+#include <stdlib.h>
+
 
 int main () {
     // TODO
+    const char* filename = "systemcalls_output1.txt";
+
+    int file = open(filename, O_TRUNC);
+    if (file < 0) {
+        perror("Appel systeme open a echoue");
+        return EXIT_FAILURE;
+    }
+
+    const char* message = "Saisissz votre texte suivi de CTRL-D : \n";
+    printf(message);
+    
+    char buffer; 
+    ssize_t size;
+
+    while((size = read(STDIN_FILENO, buffer, 1)) > 0) {
+        write(file, buffer, size);
+    }
+
+    close(file);
+
     return 0;
 }
